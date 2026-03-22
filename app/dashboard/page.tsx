57ea03cef073c2c0319b5f5ad15f4227592c1877
Xiaoting Xing <xiaotingxing2@gmail.com>
2026-03-22 17:36:33 +0800

    fix: percent undefined type error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -172,7 +172,7 @@ export default function Dashboard() {
                           cx="50%"
                           cy="50%"
                           labelLine={false}
-                          label={({ name, value, percent }) => `${name} ${value} (${(percent * 100).toFixed(0)}%)`}
+                         label={({ name, value, percent }) => `${name} ${value} (${((percent ?? 0) * 100).toFixed(0)}%)`}
                           outerRadius={80}
                           fill="#8884d8"
                           dataKey="value"
